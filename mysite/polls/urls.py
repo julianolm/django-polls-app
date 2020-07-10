24c67bc1c59cdd535185bc792cc5d2f880bf6dc6
u_equipe_03 <julian@email.com>
2020-07-10 19:07:16 -0300

    updated thanks.html
    Completed the thanks.html file and also integrated it to the app: the vote view now redirects to ThanksView, not to ResultsView anymore

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -9,3 +9,8 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# This complement on url_patterns comes from the addintion of new features
+urlpatterns += [
+    path('<int:pk>/thanks/', views.ThanksView.as_view(), name='thanks'),
+]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -42,4 +42,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after succesfully dealing
         # with POST data. This prevents data from being posted twice if a 
         # user hits the Back button
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:thanks', args=(question.id,)))
